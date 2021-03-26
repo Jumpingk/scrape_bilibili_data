46721bf09aee3feb1f325bf0e1b167b119f3f35b
Jumpingk <2816504325@qq.com>
2021-03-26 23:57:56 +0800

    commit code

--- popular_weekly.py
+++ popular_weekly.py
@@ -5,7 +5,7 @@ B站每周必看网页数据：
 WEB URL: https://www.bilibili.com/v/popular/weekly
 '''
 base_url = 'https://www.bilibili.com/v/popular/weekly'
-api_url = 'https://api.bilibili.com/x/web-interface/popular/series/one?number=104'
+api_url = 'https://api.bilibili.com/x/web-interface/popular/series/one?number={periods}'
 
 proxies = {
     'http': '116.117.134.134:80',
@@ -26,9 +26,28 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
 }
 
-response = requests.get(url=api_url, headers=headers, proxies=proxies)
+response = requests.get(url=api_url.format(periods=104), headers=headers, proxies=proxies)
 print(response.status_code)
 
-datas = response.json()['data']['list']
-for data in datas:
-    print(data['title'])
+datas =  response.json()['data']
+datas_config = datas['config']
+datas_list = datas['list']
+
+# 要获取的信息如下：
+# 期数  | 期刊发布日期 | 标题  | 作品发布日期 | up主   | 播放量       | 点赞   | 弹幕量               | 视频标签     | 视频链接
+# priods | priods_date | title | works_date | author | view_counts | like | bullet_screen_counts | video_label | video_link
+for d in datas_list:
+    data = {
+        'priods': datas_config['number'],
+        'priods_date': datas_config['stime'],
+        'title': d['title'],
+        'works_date': d['ctime'],
+        'author': d['owner']['name'],
+        'view_counts': d['stat']['view'],
+        'like': d['stat']['like'],
+        'bullet_screen_counts': d['stat']['danmaku'],
+        'video_label': d['rcmd_reason'],
+        'video_link': d['short_link']
+    }
+    print(data)
+
